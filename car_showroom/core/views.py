cfbd67a0f7558b66b269c5bbe9f279a24ac65958
nastechka <anfirunovich@gmail.com>
2023-02-10 16:11:34 +0300

    Add filterset_class

--- car_showroom/core/views.py
+++ car_showroom/core/views.py
@@ -3,6 +3,8 @@ from rest_framework.viewsets import ModelViewSet
 
 from core.serializers import CarSerializer, LocationSerializer
 
+from core.filters.car_filter import CarFilter
+
 from core.models import Car, Location
 
 
@@ -11,6 +13,8 @@ class CarViewSet(ModelViewSet):
     serializer_class = CarSerializer
     queryset = Car.objects.filter(is_active=True).all()
 
+    filterset_class = CarFilter
+
     permission_classes = (permissions.IsAuthenticated,)
 
 
@@ -18,5 +22,7 @@ class LocationViewSet(ModelViewSet):
 
     serializer_class = LocationSerializer
     queryset = Location.objects.filter(is_active=True).all()
+
+    filterset_class = CarFilter
 
     permission_classes = (permissions.IsAuthenticated,)
